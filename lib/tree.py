e7b921ac0eff0c94424dfffdb1a989715d0ab8b1
sedatDemiriz <sedatdemiriz97@gmail.com>
2025-02-15 23:39:35 -0500

    Add typehints for Tree class

--- lib/tree.py
+++ lib/tree.py
@@ -6,8 +6,8 @@ class Tree:
     def __init__(self, results: list[item.Item]):
         self.results = results
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Crafting tree that makes {self.results}"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Tree(results={self.results})"
