85487d46e680db536bcde0ec6cae04981181e2af
sedatDemiriz <sedatdemiriz97@gmail.com>
2025-02-15 23:38:55 -0500

    Add docstrings and attribute getters plus tests for Item class

--- lib/item.py
+++ lib/item.py
@@ -3,6 +3,9 @@ class Item:
     __states = ["solid", "liquid", "gas"]
 
     def __init__(self, name: str, state: str, rate: float, is_raw: bool):
+        """
+        Instantiate item
+        """
         self.name = name
         self.rate = rate
         self.is_raw = is_raw
@@ -12,8 +15,26 @@ class Item:
 
         self.state = state
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Return string representation of item
+        """
         return f"{self.rate} of item {self.name} in state {self.state}"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """
+        Return string representation of item call
+        """
         return f"Item(name={self.name}, rate={self.rate}, state={self.state})"
+
+    def item_is_raw(self) -> bool:
+        """
+        Return whether the item is a raw input (resource)
+        """
+        return self.is_raw
+
+    def item_has_name(self, name_query) -> bool:
+        """
+        Return whether the item has the queried name (resource)
+        """
+        return self.name == name_query
--- tests/test_item.py
+++ tests/test_item.py
@@ -2,7 +2,7 @@ import unittest
 from lib import item
 
 
-class TestItem(unittest.TestCase):
+class TestItemInstantiation(unittest.TestCase):
 
     name = "Test Name"
     state = "solid"
@@ -35,6 +35,44 @@ class TestBadState(unittest.TestCase):
             item.Item(
                 name=self.name, state=self.state, rate=self.rate, is_raw=self.is_raw
             )
+
+
+class TestIsRaw(unittest.TestCase):
+
+    name = "Test Name"
+    state = "solid"
+    rate = 100
+
+    def test_is_raw(self):
+        test_item = item.Item(
+            name=self.name, state=self.state, rate=self.rate, is_raw=True
+        )
+        self.assertTrue(test_item.item_is_raw())
+
+    def test_is_not_raw(self):
+        test_item = item.Item(
+            name=self.name, state=self.state, rate=self.rate, is_raw=False
+        )
+        self.assertFalse(test_item.item_is_raw())
+
+
+class TestHasName(unittest.TestCase):
+
+    state = "solid"
+    rate = 100
+    is_raw = True
+
+    def test_has_name(self):
+        test_item = item.Item(
+            name="TESTING", state=self.state, rate=self.rate, is_raw=self.is_raw
+        )
+        self.assertTrue(test_item.item_has_name("TESTING"))
+
+    def test_doesnt_have_name(self):
+        test_item = item.Item(
+            name="NOT TESTING", state=self.state, rate=self.rate, is_raw=self.is_raw
+        )
+        self.assertFalse(test_item.item_has_name("TESTING"))
 
 
 if __name__ == "__main__":
