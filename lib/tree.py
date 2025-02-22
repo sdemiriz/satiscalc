9b3fcf1e3616ac8f812cba2da8a95151c3eb310f
sedatDemiriz <sedatdemiriz97@gmail.com>
2025-02-21 23:06:21 -0500

    add additional initialization parameters for available items and recipes

--- lib/tree.py
+++ lib/tree.py
@@ -1,13 +1,23 @@
-from lib import item
+from lib import item, recipe
 
 
 class Tree:
 
-    def __init__(self, results: list[item.Item]):
+    def __init__(self, results: list[item.Item], recipes: list[recipe.Recipe], items: list[item.Item]):
         self.results = results
+        self.recipes = recipes
+        self.items = items
+        #self.machines = machines
 
     def __str__(self) -> str:
         return f"Crafting tree that makes {self.results}"
 
     def __repr__(self) -> str:
         return f"Tree(results={self.results})"
+    
+    def save_tree(self) -> None:
+        pass
+
+    def load_tree(self):
+        pass
+
