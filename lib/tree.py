8d7e7d6e0ea7ff66a6a4593227647046e58a94d6
sedatDemiriz <sedatdemiriz97@gmail.com>
2025-02-22 00:10:28 -0500

    Add crafting Step support to Tree class

--- lib/tree.py
+++ lib/tree.py
@@ -1,4 +1,4 @@
-from lib import item, recipe
+from lib import item, recipe, step
 
 
 class Tree:
@@ -9,15 +9,27 @@ class Tree:
         self.items = items
         #self.machines = machines
 
+        self.root = []
+
     def __str__(self) -> str:
         return f"Crafting tree that makes {self.results}"
 
     def __repr__(self) -> str:
-        return f"Tree(results={self.results})"
+        return f"Tree(results={self.results}, recipes={self.recipes}, items={self.items})"
     
-    def save_tree(self) -> None:
+    def save(self, filename) -> None:
         pass
 
-    def load_tree(self):
+    def load_tree(self, filename):
         pass
+
+    def filter_recipes(self, output: item.Item) -> list[recipe.Recipe]:
+        return [r for r in self.recipes if r.has_output(output)]
+
+    def build_tree(self):
+        for result in self.results:
+            self.root.append(step.Step(goal=result, recipes=self.recipes, items=self.items))
+
+        for child in self.root:
+            child.calculate_children()
 
